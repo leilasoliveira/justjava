f0c0744b045cf6fa59a6a9aec0ed5c95059f2c2f
Leila S. Oliveira <leila@opcaogourmet.com.br>
2016-07-17 22:22:03 -0300

    add variable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2 *5);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
