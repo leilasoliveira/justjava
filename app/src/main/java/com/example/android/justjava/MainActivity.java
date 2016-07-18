1aca0aba705a8db24bd4be7cecef87e6cf84fbf2
Leila S. Oliveira <leila@opcaogourmet.com.br>
2016-07-17 23:59:03 -0300

    pretty price message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,8 +24,10 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: " + price;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     public void increment(View view){
@@ -36,6 +38,14 @@ public class MainActivity extends ActionBarActivity {
     public void decrement(View view){
         quantity = quantity - 1;
         display(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
